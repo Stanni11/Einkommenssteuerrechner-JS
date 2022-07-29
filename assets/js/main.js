ea99906e4202311354a32216d41e2d5cd4a3192b
Stanni11 <supercoder@SuperCoders-Air.fritz.box>
2022-07-29 17:04:10 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const yearInput = document.getElementById('yearInput')
+const incomeInput = document.getElementById('incomeInput')
+
+const select_no = document.getElementById('select_no')
+
+const select_8 = document.getElementById('select_8')
+
+const select_9 = document.getElementById('select_9')
+
+const select_ja = document.getElementById('select_ja')
+
+const select_nein = document.getElementById('select_nein')
+
